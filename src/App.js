841cacee43d732dfef2e18482d9855ae23d21948
Afrosiyob <afrosiyob.a97@gmail.com>
2020-08-07 10:03:21 +0500

    adadad
    adad

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import Routes from './Routes';
-import i18n from './i18n';
 
 
 
-function App({t}) {
+
+function App() {
   return (
     <div className="App">
 
-<h1>{t('Welcome to React')}</h1>
+
 
       <Router>
         <Link to='/' > page one </Link>
         <Link to='/pagetwo' > page two </Link>
 
-        <Routes/>
+        <Routes />
       </Router>
 
-     
+
     </div>
   );
 }
